18d1bc8ec92a1a34a0328b42a0f62f9895bb36f7
emjin <emmaljin@yahoo.com>
2015-07-09 14:02:07 -0400

    Animates the Correct/Incorrect message

--- app/src/main/java/com/mbhs/analemma/helloworld/DisplayMessageActivity.java
+++ app/src/main/java/com/mbhs/analemma/helloworld/DisplayMessageActivity.java
@@ -1,8 +1,10 @@
 package com.mbhs.analemma.helloworld;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -28,11 +30,22 @@ public class DisplayMessageActivity extends ActionBarActivity {
             returnMes = "The Analemma smiles upon you with its rays of infinity.";
         else
             returnMes = "Incorrect |-|-|";
+
+        //Set the textView as the activity layout
         TextView textView = (TextView) findViewById(R.id.return_mes);
         textView.setTextSize(40);
         textView.setText(returnMes);
-        //Set the textView as the activity layout
 
+        //Get size of window
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int xCorner = size.x - textView.getWidth();
+        int yCorner = size.y - textView.getHeight();
+
+        //Animate--in theory, first one flips it and moves to the corner, second one moves it back
+        textView.animate().x(xCorner).y(yCorner).rotationYBy(720);
+        textView.animate().x(0).y(0);
     }
 
     @Override
